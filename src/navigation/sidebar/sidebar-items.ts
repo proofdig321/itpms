44b3585908973e593cfbc06b0627b04924172d76
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2026-06-17 21:40:55 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/navigation/sidebar/sidebar-items.ts
@@ -0,0 +1,175 @@
+import {
+  Banknote,
+  Calendar,
+  ChartBar,
+  Fingerprint,
+  Forklift,
+  Gauge,
+  GraduationCap,
+  Kanban,
+  LayoutDashboard,
+  ListTodo,
+  Lock,
+  type LucideIcon,
+  Mail,
+  MessageSquare,
+  ReceiptText,
+  ShoppingBag,
+  SquareArrowUpRight,
+  Users,
+} from "lucide-react";
+
+export interface NavSubItem {
+  title: string;
+  url: string;
+  icon?: LucideIcon;
+  comingSoon?: boolean;
+  newTab?: boolean;
+  isNew?: boolean;
+}
+
+export interface NavMainItem {
+  title: string;
+  url: string;
+  icon?: LucideIcon;
+  subItems?: NavSubItem[];
+  comingSoon?: boolean;
+  newTab?: boolean;
+  isNew?: boolean;
+}
+
+export interface NavGroup {
+  id: number;
+  label?: string;
+  items: NavMainItem[];
+}
+
+export const sidebarItems: NavGroup[] = [
+  {
+    id: 1,
+    label: "Dashboards",
+    items: [
+      {
+        title: "Default",
+        url: "/dashboard/default",
+        icon: LayoutDashboard,
+      },
+      {
+        title: "CRM",
+        url: "/dashboard/crm",
+        icon: ChartBar,
+      },
+      {
+        title: "Finance",
+        url: "/dashboard/finance",
+        icon: Banknote,
+      },
+      {
+        title: "Analytics",
+        url: "/dashboard/analytics",
+        icon: Gauge,
+      },
+      {
+        title: "Productivity",
+        url: "/dashboard/productivity",
+        icon: ListTodo,
+      },
+      {
+        title: "E-commerce",
+        url: "/dashboard/ecommerce",
+        icon: ShoppingBag,
+      },
+      {
+        title: "Academy",
+        url: "/dashboard/academy",
+        icon: GraduationCap,
+        isNew: true,
+      },
+      {
+        title: "Logistics",
+        url: "/dashboard/logistics",
+        icon: Forklift,
+      },
+    ],
+  },
+  {
+    id: 2,
+    label: "Pages",
+    items: [
+      {
+        title: "Email",
+        url: "/dashboard/mail",
+        icon: Mail,
+      },
+      {
+        title: "Chat",
+        url: "/dashboard/chat",
+        icon: MessageSquare,
+      },
+      {
+        title: "Calendar",
+        url: "/dashboard/calendar",
+        icon: Calendar,
+      },
+      {
+        title: "Kanban",
+        url: "/dashboard/kanban",
+        icon: Kanban,
+      },
+      {
+        title: "Invoice",
+        url: "/dashboard/invoice",
+        icon: ReceiptText,
+      },
+      {
+        title: "Users",
+        url: "/dashboard/users",
+        icon: Users,
+      },
+      {
+        title: "Roles",
+        url: "/dashboard/roles",
+        icon: Lock,
+      },
+      {
+        title: "Authentication",
+        url: "/auth",
+        icon: Fingerprint,
+        subItems: [
+          { title: "Login v1", url: "/auth/v1/login", newTab: true },
+          { title: "Login v2", url: "/auth/v2/login", newTab: true },
+          { title: "Register v1", url: "/auth/v1/register", newTab: true },
+          { title: "Register v2", url: "/auth/v2/register", newTab: true },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    label: "Legacy",
+    items: [
+      {
+        title: "Dashboards",
+        url: "/dashboard/default-v1",
+        subItems: [
+          { title: "Default V1", url: "/dashboard/default-v1" },
+          { title: "CRM V1", url: "/dashboard/crm-v1" },
+          { title: "Finance V1", url: "/dashboard/finance-v1" },
+          { title: "Analytics V1", url: "/dashboard/analytics-v1" },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    label: "Misc",
+    items: [
+      {
+        title: "Others",
+        url: "/dashboard/coming-soon",
+        icon: SquareArrowUpRight,
+        comingSoon: true,
+      },
+    ],
+  },
+];
