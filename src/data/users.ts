06bfbdebbe15a643b3bd4cf0c8f3051c18c654bb
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2026-06-18 00:04:51 +0000

    chore: update dashboard user to Bheki Simelane

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,19 +1,11 @@
 export const users = [
   {
     id: "1",
-    name: "Arham Khan",
-    username: "Aarhamkhnz",
-    email: "hello@arhamkhnz.com",
-    avatar: "https://avatars.githubusercontent.com/u/43849669",
-    role: "administrator",
-  },
-  {
-    id: "2",
-    name: "Ammar Khan",
-    username: "ammarkhnz",
-    email: "hello@ammarkhnz.com",
+    name: "Bheki Simelane",
+    username: "bhekisimelane",
+    email: "info@unamifoundation.org",
     avatar: "",
-    role: "admin",
+    role: "administrator",
   },
 ];
 
